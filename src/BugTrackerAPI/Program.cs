887d8df0e5a7c00824cbd1ac58dff16b35d9334b
ezbishal <ezbishal@gmail.com>
2023-11-29 21:36:24 +0000

    Misc changes

--- src/BugTrackerAPI/Program.cs
+++ src/BugTrackerAPI/Program.cs
@@ -4,77 +4,10 @@ using BugTrackerApi.Features.GetAllProjects;
 using BugTrackerApi.Features.GetProjectById;
 using BugTrackerApi.Features.Token;
 using BugTrackerApi.Features.UpdateProject;
-using BugTrackerAPI.Data;
-using BugTrackerAPI.Helpers;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
+using BugTrackerAPI;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddCors();
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Rhenus External API", Version = "v1" });
-
-    // Define the OAuth2.0 or Bearer token scheme
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "JWT Authorization header using the Bearer scheme.",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    },
-                    Scheme = "oauth2",
-                    Name = "Bearer",
-                    In = ParameterLocation.Header,
-                },
-                new List<string>()
-            }
-        });
-});
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = "JwtBearer";
-    options.DefaultChallengeScheme = "JwtBearer";
-})
-    .AddJwtBearer("JwtBearer", JwtBearerOptions =>
-    {
-        JwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTellAnyoneAboutIt")),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.FromMinutes(5)
-        };
-
-    });
-builder.Services.AddAuthorization();
-builder.Services.AddCors();
-
-builder.Services.AddDbContext<ApplicationDbContext>();
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddAutoMapper(typeof(MapperProfiles));
-
+builder.Services.ConfigureServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -87,9 +20,11 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseExceptionHandler(_ => { });
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCors();
 
 //Token
 app.MapGroup("/api/token").WithTags("Token")
@@ -108,6 +43,5 @@ app.MapGroup("/api/projects").WithTags("Projects")
 
 // Comments
 
-app.UseCors();
 app.Run();
 
---
+++ src/BugTrackerAPI/Registry.cs
@@ -0,0 +1,83 @@
+﻿using BugTrackerAPI.Data;
+using BugTrackerAPI.Helpers;
+using FluentValidation;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace BugTrackerAPI;
+
+public static class Registry
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddCors();
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Rhenus External API", Version = "v1" });
+
+            // Define the OAuth2.0 or Bearer token scheme
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = "JWT Authorization header using the Bearer scheme.",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header,
+                },
+                new List<string>()
+            }
+        });
+        });
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = "JwtBearer";
+            options.DefaultChallengeScheme = "JwtBearer";
+        })
+            .AddJwtBearer("JwtBearer", JwtBearerOptions =>
+            {
+                JwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTellAnyoneAboutIt")),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(5)
+                };
+
+            });
+        services.AddAuthorization();
+
+        services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddValidatorsFromAssemblyContaining(typeof(Program));
+
+        services.AddCors();
+
+        services.AddDbContext<ApplicationDbContext>();
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddAutoMapper(typeof(MapperProfiles));
+
+        return services;
+    }
+}
