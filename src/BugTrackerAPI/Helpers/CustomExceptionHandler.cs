181a8a39722d7e6c8da1002e122957f0b2429e1e
ezbishal <ezbishal@gmail.com>
2023-11-29 21:38:26 +0000

    Misc changes

---
+++ src/BugTrackerAPI/Helpers/CustomExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace BugTrackerAPI.Helpers;
+
+public class CustomExceptionHandler : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
