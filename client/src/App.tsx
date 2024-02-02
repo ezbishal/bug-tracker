3dc7c55cafd05e5221970c1b0a2ef4dc02dbc294
Bishow Adhikari <bishow712@gmail.com>
2024-02-02 17:49:26 +0545

    NavigationIssue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Dashboard from './Components/Dashboard';
@@ -8,21 +8,28 @@ const setToken = (userToken: string): void => {
   sessionStorage.setItem('token', userToken);
 }
 
-const getToken = (): string => {
-  const token: string | null = sessionStorage.getItem('token');
-  return token ?? '';
-}
-
 const App: FC = () => {
+
+  const[token, setToken] = useState<string | null>(null)
+
+  useEffect(()=>{
+    const getToken = async (): Promise<void> => {
+      const fetchedToken: string | null = await sessionStorage.getItem('token');
+      setToken(fetchedToken || null);
+    }
+
+    getToken()
+  }, [])
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='login' element={ getToken() !== '' ? <Navigate replace to={'/'} /> : <Login setToken={setToken} />} />
-        <Route path='/' element={ getToken() !== '' ? <Dashboard /> : <Navigate replace to={'/login'} />} />
+        <Route path='/login' element={ token !== null ? <Navigate replace to={'/'} /> : <Login setToken={setToken} />} />
+        <Route path='/' element={ token !== null ? <Dashboard /> : <Navigate replace to={'/login'} />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
